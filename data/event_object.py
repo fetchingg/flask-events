d50833b616df325fa40f1154df9aa56b8615330a
Integral <kumbaydve@gmail.com>
2026-05-12 22:21:41 +0300

    user & event nullable update & gitignore

--- data/event_object.py
+++ data/event_object.py
@@ -5,11 +5,12 @@ from data.db_session import SqlAlchemyBase
 class Event(SqlAlchemyBase):
     __tablename__ = 'events'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    description = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    category = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     place = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     creator_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'), nullable=False)
-    image_path = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    image_path = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
