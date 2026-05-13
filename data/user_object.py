734362991f66ac4f7513fac5ffa623a871e7cc41
Integral <kumbaydve@gmail.com>
2026-05-13 17:35:37 +0300

    event api (get all & post)

--- data/user_object.py
+++ data/user_object.py
@@ -1,8 +1,10 @@
 import sqlalchemy
+from sqlalchemy_serializer import SerializerMixin
+
 from data.db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
---
+++ test.py
@@ -0,0 +1,3 @@
+from requests import get, post
+
+print(post('http://127.0.0.1:5000/api/events'))
